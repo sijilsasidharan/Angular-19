16b9f9c41dbc507ac156fa673fab7950c910ab80
sijilsasidharan <sijilsasidharan@gmail.com>
2025-03-14 23:16:31 +0530

    Sidenav content component

---
+++ src/app/core/sidenav/sidenav-content/sidenav-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-content',
+  imports: [],
+  templateUrl: './sidenav-content.component.html',
+  styleUrl: './sidenav-content.component.scss'
+})
+export class SidenavContentComponent {
+
+}
