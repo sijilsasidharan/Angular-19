b6cc045c52ecdea159e99c4c391da5d3425c938f
sijilsasidharan <sijilsasidharan@gmail.com>
2025-03-15 21:02:50 +0530

    Sidenav content updates

--- src/app/core/sidenav/sidenav-content/sidenav-content.component.ts
+++ src/app/core/sidenav/sidenav-content/sidenav-content.component.ts
@@ -4,8 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-sidenav-content',
   imports: [],
   templateUrl: './sidenav-content.component.html',
-  styleUrl: './sidenav-content.component.scss'
+  styleUrl: './sidenav-content.component.scss',
 })
 export class SidenavContentComponent {
-
+  menuItems = [
+    { name: 'Home', route: '/home', icon: 'home' },
+    { name: 'About', route: '/about', icon: 'info' },
+    { name: 'Contact', route: '/contact', icon: 'contact' },
+  ];
 }
