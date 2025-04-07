10164b05ecf41a181c9bc8a8241d8f6c8b80b9e0
sijilsasidharan <sijilsasidharan@gmail.com>
2025-04-07 23:40:52 +0530

    Router updates

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,25 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Route } from '@angular/router';
 
-export const serverRoutes: ServerRoute[] = [
+export const serverRoutes: Route[] = [
+  {
+    path: '/home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+
+  {
+    path: '/products',
+    loadComponent: () =>
+      import('./features/products/products.component').then(
+        (m) => m.ProductsComponent
+      ),
+  },
+  {
+    path: '/about',
+    loadComponent: () =>
+      import('./features/about/about.component').then((m) => m.AboutComponent),
+  },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  },
 ];
