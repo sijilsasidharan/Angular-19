94e1da7368f25174334cdf5573410eb248c09188
sijilsasidharan <sijilsasidharan@gmail.com>
2025-04-08 23:35:26 +0530

    Server Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {} from '@angular/router';
+import { ServerRoute } from '@angular/ssr';
 
-export const routes: Routes = [];
+export const routes: ServerRoute[] = [
+  {
+    path: '/home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+];
